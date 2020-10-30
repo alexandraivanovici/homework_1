64d8de875efbad2d6fa29b3a645270d6c839db42
alexandraivanovici <ivanoviciioana18@stud.ase.ro>
2020-10-30 22:58:03 +0200

    tema 1

--- main/app.js
+++ main/app.js
@@ -1,5 +1,27 @@
 function removeOrderItem(orderInfo, position){
     
+    if(Array.isArray(orderInfo.items)==false) {
+        throw new Error('Items should be an array');
+    }
+
+
+    for(let i =0; i < orderInfo.items.length; i++){
+        if(orderInfo.items[i].price == undefined || orderInfo.items[i].quantity == undefined){
+            throw new Error('Malformed item');
+        }
+    }
+
+    if( position < 0 || position > orderInfo.items.length - 1){
+        throw new Error('Invalid position');
+    }
+
+    let t = orderInfo.items[position].price * orderInfo.items[position].quantity;
+    
+    orderInfo.total = orderInfo.total - t;
+
+    orderInfo.items.splice(position, 1);
+        
+    return orderInfo;
 }
 
 const app = {
